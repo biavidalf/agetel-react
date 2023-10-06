2b3af66ac51dc69dae350c31471f8eb01cbbe2dc
Beatriz Vidal <74744158+biavidalf@users.noreply.github.com>
2023-10-06 15:51:48 -0300

    :truck: chore: Base project structure

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import Home from './pages/Home'
+import Landing from './pages/Landing'
+
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Landing />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+)
